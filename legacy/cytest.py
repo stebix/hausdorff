7f8a99f257b4b4e20b5483b52e27ec3495fe8fb3
Jannik Stebani <jannik.stebani@physik.uni-wuerzburg.de>
2022-08-26 03:59:22 +0200

    Remove legacy cruft

--- legacy/cytest.py
+++
@@ -1,9 +0,0 @@
-import dhd
-import numpy as np
-
-a = np.arange(0, 9, dtype=np.float64).reshape(3, 3)
-b = np.arange(0, 9, dtype=np.float64).reshape(3, 3)
-
-
-result = dhd.mysum(a, b)
-print(result)
